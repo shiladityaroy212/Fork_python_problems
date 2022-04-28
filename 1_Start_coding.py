88c1d853ca095aeabd56a7bd2fbda90b2aea815e
Shiladitya Roy <53702952+shiladityaroy212@users.noreply.github.com>
2022-04-28 10:38:59 +0530

    Create 1_Start_coding.py

---
+++ 1_Start_coding.py
@@ -0,0 +1,6 @@
+# When learning a new language, we first learn to output some message. Here, we'll start with the famous "Hello World" message. 
+# Now, here you are given a function to complete. Don't worry about the ins and outs of functions, just add the command (print ("Hello World")) to print Hello World.
+# Your Task: Your task is to complete the function print_fun() to print hello world.
+
+def print_fun():
+    print("Hello World")
