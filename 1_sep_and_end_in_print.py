2a37e35d180c33457a8cc52b4b41b45b39569bbe
Shiladitya Roy <53702952+shiladityaroy212@users.noreply.github.com>
2022-04-29 10:31:23 +0530

    Create 1_sep_and_end_in_print.py

---
+++ 1_sep_and_end_in_print.py
@@ -0,0 +1,8 @@
+''' You are familiar with producing output using Python. In this task, you'll be required to write message separated by and ending with '$'. Write Geeks$For$Geeks$. 
+But we would be using end and sep parameters of print() to do so.
+
+Output:
+Geeks$For$Geeks$  '''
+
+def print_func(string1, string2):
+    print ("Geeks","For","Geeks", sep = '$', end = '$')
