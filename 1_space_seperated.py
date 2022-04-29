8376d3859bb6242ed83866852898d5b815954dcd
Shiladitya Roy <53702952+shiladityaroy212@users.noreply.github.com>
2022-04-29 10:28:34 +0530

    Create 1_space_seperated.py

---
+++ 1_space_seperated.py
@@ -0,0 +1,14 @@
+''' You are familiar with producing output using Python. In this task, you'll be required to write message separated by space. Write Geeks For Geeks. But wait a minute!! 
+You need to write each word separate by space without using space in string.
+In the function print_func(), output each word of Geeks for Geeks separated by space.
+
+Hint: You can use ',' for separating words by spaces like print("py","thon") outputs py thon
+
+Example:
+Output: 
+Geeks For Geeks 
+Explanation: Each word of the statement is separated by space.
+Your Task: Your task is to print "Geeks For Geeks". '''
+
+def print_fun():
+   print("Geeks","For","Geeks")
