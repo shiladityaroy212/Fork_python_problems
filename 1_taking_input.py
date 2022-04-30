df72e96e1aeb7e036306227d6a1089207fde2ea7
Shiladitya Roy <53702952+shiladityaroy212@users.noreply.github.com>
2022-04-30 10:51:42 +0530

    Create 1_taking_input.py

---
+++ 1_taking_input.py
@@ -0,0 +1,15 @@
+''' In Python, we use the input() function to take input and assign the input to a variable. In Python, a variable name is not preceded by its data-type, instead we just 
+use the variable name and python changes its data-type according to the input type.
+Example:
+
+Input:
+a = Geeks
+b = For
+c = Geeks
+Output:  Geeks For Geeks  '''
+
+def inPutCat():
+    a = input()
+    b = input()
+    c = input()
+    print(a, b, c)
