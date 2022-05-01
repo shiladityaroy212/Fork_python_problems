08bcb89cbd012f3290f1e5d067288c49169a4036
Shiladitya Roy <53702952+shiladityaroy212@users.noreply.github.com>
2022-05-01 22:10:57 +0530

    Create 1_comment_in_python.py

---
+++ 1_comment_in_python.py
@@ -0,0 +1,16 @@
+''' You have learnt about input and output in Python. One of the pivotal thing in the world of programming is comment which is used by developers to describe their code 
+or to preclude compiler from compiling the line(s) which they don't want to. In this section, we will learn how to comment any line in Python.
+
+Hint:
+# -> is used to comment a single line.
+''' ''' -> is used to comment multiple lines.
+
+Example:
+Output: Geeks For Geeks  '''
+
+def print_func():
+   # print ("Geeks Classes")
+    print ("Geeks For Geeks")
+    #print ("Geeks Quiz")
+    #print ("Geeks")
+    #print ("Geeks For")
